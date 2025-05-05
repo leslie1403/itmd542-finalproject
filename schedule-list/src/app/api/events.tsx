01df47a66905079b2a668d4c7e269cfa277ddbce
leslie1403 <lesquivel@hawk.iit.edu>
2025-05-05 18:24:58 -0500

    fixed formatting, can now add events

--- schedule-list/src/app/api/events.tsx
+++ schedule-list/src/app/api/events.tsx
@@ -4,19 +4,29 @@ import { sql } from './database';
 import { revalidatePath } from 'next/cache';
 
 export type EventRow = {
-  id: string;
-  title: string;
-  location: string;
-  description: string | null;
-  start_time: string;
-  end_time: string;
+    id: string;
+    title: string;
+    location: string;
+    description: string | null;
+    start_time: string;
+    end_time: string;
 };
 
 export async function getEvents(): Promise<EventRow[]> {
-  const{ rows } = 
-    await sql<EventRow>`SELECT * FROM events ORDER BY start_time ASC`;
-  return rows;
+    const { rows } =
+        await sql<EventRow>`SELECT * FROM events ORDER BY start_time ASC`;
+    return rows;
 }
-export async function createEvent(_: FormData) {}
-export async function updateEvent(_: FormData) {}
-export async function deleteEvent(_: FormData) {}
+
+export async function createEvent(form: FormData) {
+    const { title, location, description, start, end } = Object.fromEntries(form) as Record<string, string>;
+
+    await sql`
+    INSERT INTO events (title, location, description, start_time, end_time)
+    VALUES (${title}, ${location}, ${description || null}, ${start}, ${end})
+     `;
+    revalidatePath('/');
+}
+
+export async function updateEvent(_: FormData) { }
+export async function deleteEvent(_: FormData) { }
--- schedule-list/src/app/page.tsx
+++ schedule-list/src/app/page.tsx
@@ -1,19 +1,28 @@
-import { getEvents } from './api/events';
+import { getEvents,createEvent } from './api/events';
+import { format } from 'date-fns';
 
 export default async function Home() {
-  const events = await getEvents();  
+  const events = await getEvents();
 
   return (
     <main>
       <h1>Schedule List</h1>
 
+      <form action={createEvent}> 
+          <input name="title" placeholder="Title" required />
+          <input name="location" placeholder="Location" required />
+          <input type="datetime-local" name="start" required />
+          <input type="datetime-local" name="end" required />
+          <button type="submit">Add</button>
+        </form>
+
       {events.length === 0 ? (
-        <p >Placeholder - Test</p>
+        <p>Placeholder - Test</p>
       ) : (
         <ul>
           {events.map((e) => (
             <li key={e.id}>
-              {e.title} — {e.location} ({e.start_time} → {e.end_time})
+              {e.title} — {e.location}{''} ({format(new Date(e.start_time), 'PPpp')} {'→'} ({format(new Date(e.end_time), 'PPpp')})
             </li>
           ))}
         </ul>
